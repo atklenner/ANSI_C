daac5b5bd03d27a6489755c835e91ce972d87bfd
Andrew Klenner <atklenner@gmail.com>
2022-09-06 21:28:09 -0700

    messing with numbers

---
+++ learn_c/numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int foo;
+    int bar = 1;
+    printf("%d\n", foo); // who knows
+    printf("%d\n", bar);
+
+    int a = 0, b = 1, c = 2, d = 3, e = 4;
+    a = b - c + d * e;
+    printf("%d\n", a);
+    int sum = a + b + c;
+    printf("%d", sum);
+}
